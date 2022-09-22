b7c240eb6735d4d778c96ed92f79dfe73739b36e
doganceylanci <19500072@ogr.dicle.edu.tr>
2022-09-23 01:24:12 +0300

    some issues have been fixed

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import requests as r 
-from bs4 import BeautifulSoup as bs
-import pandas as pd
-
-# Get the data from the website
-cins=str(input("döviz cinsini girin (dolar/euro): ")).lower().capitalize()
-url =f"https://www.bloomberght.com/doviz/{cins.lower()}"
-params = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-with r.session() as s:
-    page = s.get(url, params=params)
-    soup = bs(page.content, "html.parser")
-    fiyat = soup.find("span", {"class":"LastPrice upGreen"}).text
-    degisim = soup.find("span", {"class":"bulk PercentChange"}).text
-    tarih = soup.find("span", {"class":"date"}).text
-    df = pd.DataFrame({"Cins":cins ,"Fiyat":fiyat, "Degisim":degisim, "Tarih        Saat":tarih}, index=[1])
-    print(df)
+from packages import yakit
+from packages import doviz
+if __name__ == "__main__":
+    print(doviz.fiyatOgren())
+    print(yakit.fiyatlar())
