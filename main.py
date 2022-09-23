13eaae3fd95b9f59523366b75824972afeed7b7f
doganceylanci <19500072@ogr.dicle.edu.tr>
2022-09-23 22:05:56 +0300

    some issues have been fixed

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from packages import yakit
 from packages import doviz
 if __name__ == "__main__":
     print("\n")
-    doviz.fiyatOgren()
+    try:
+        doviz.fiyatOgren()
+    except:
+        print("Döviz verileri çekilemedi. yanlış bir şeyler var. Lütfen doğru girildiğinden emin olun.")
     print("\n")
     yakit.fiyatlar()
