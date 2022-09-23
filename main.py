2a7d167941b872c4ee2cc636c70ef0b6f2532a43
doganceylanci <19500072@ogr.dicle.edu.tr>
2022-09-23 22:02:03 +0300

    some issues have been fixed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from packages import yakit
 from packages import doviz
 if __name__ == "__main__":
-    print(doviz.fiyatOgren())
-    print(yakit.fiyatlar())
+    print("\n")
+    doviz.fiyatOgren()
+    print("\n")
+    yakit.fiyatlar()
